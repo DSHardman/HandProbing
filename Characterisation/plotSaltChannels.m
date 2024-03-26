18d4e7539165e15bbc1dee7685782ff9cb5283cf
David <dshardman@outlook.com>
2024-03-26 12:28:08 +0000

    supplementary structure

--- Characterisation/plotSaltChannels.m
+++ Characterisation/plotSaltChannels.m
@@ -10,20 +10,44 @@ temps = [20 20 21 20 22 22 21 22 22 21 21 21 19 20 23 22 23 22 20 21 21];
 hums = [58 57 58 53 50 51 49 47 45 47 46 46 48 46 42 44 48 51 36 32 30];
 
 % temporary testing
-times = times(6:end);
-meas = meas(:, 6:end);
-temps = temps(6:end);
-hums = hums(6:end);
+% times = times(6:end);
+% meas = meas(:, 6:end);
+% temps = temps(6:end);
+% hums = hums(6:end);
+
+
+clf
 
 for i = 1:6
-    subplot(3,1,1);
-    plot(times, meas(i,:));
-    hold on
-    subplot(3,1,3);
-    plot(times, meas(i,:) +60*hums.*meas(i,:));
+    subplot(2,1,1);
+    plot(times, meas(i,:), 'linewidth', 2);
     hold on
 end
-subplot(3,1,2)
-plot(times, hums);
-% subplot(3,1,3)
-% plot(times, temps);
+legend({"1"; "2"; "3"; "4"; "5"; "6"}, 'orientation' ,'horizontal', 'location', 'n');
+legend boxoff
+set(gca, 'linewidth', 2, 'fontsize', 15);
+ylabel("Resistance (k\Omega)");
+xlabel("Time (h)");
+box off
+ylim([0 500]);
+
+set(gcf, 'position', [488   154   560   604], 'color', 'w');
+
+subplot(2,1,2)
+yyaxis left
+plot(times, hums, 'linewidth', 2);
+set(gca, 'YDir', 'reverse');
+ylim([25 65]);
+set(gca, 'linewidth', 2, 'fontsize', 15);
+ylabel("Humdity (%)");
+
+yyaxis right
+plot(times, temps, 'linewidth', 2);
+ylim([17 26]);
+set(gca, 'linewidth', 2, 'fontsize', 15);
+ylabel("Temperature (^oC)");
+xlabel("Time (h)");
+box off
+
+% plot(times, meas(i,:) +60*hums.*meas(i,:));
+% hold on
